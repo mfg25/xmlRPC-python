5af312570162a48cfbeb6ebd11518c284a960e50
matheus ferreira germano <unnmfg@usp.br>
2024-06-15 00:08:03 -0300

    function added

--- client.py
+++ client.py
@@ -9,6 +9,19 @@ class ComplexType:
         self.number = number
         self.string = string
 
+class ComplexType2:
+    def __init__(self, id, age, address, name, food, color, game, sport, height, nickname):
+        self.id = id
+        self.age = age
+        self.address = address
+        self.name = name
+        self.food = food
+        self.color = color
+        self.game = game
+        self.sport = sport
+        self.height = height
+        self.nickname = nickname
+
 # Função para medir o tempo de execução
 def timed_operation(func, *args):
     start_time = time.perf_counter()
@@ -22,13 +35,28 @@ def perform_operation(operation, *args):
     result, elapsed_time = timed_operation(operation, *args)
     print(f"Operation result: {result} (Time taken: {elapsed_time:.9f} seconds)")
 
-# Chamar as funções remotas com temporizadores
-print("Calling remote operations with timers:")
-result, elapsed_time = timed_operation(server.void)
-print(f"Void operation completed in {elapsed_time:.9f} seconds")
-perform_operation(server.long_function, 100000000)
-perform_operation(server.multiple_long_function, 100000000, 100000000, 100000000, 100000000, 100000000, 100000000, 100000000, 100000000)
-perform_operation(server.string_operation, "hello world")
-complex_value = ComplexType(1, "matheus")
-perform_operation(server.complex_operation, complex_value)
-perform_operation(server.multiple_string, "hello", "world", "helloworld", "helloworld", "helloworld", "helloworld")
+for i in range (1,10):
+    # Chamar as funções remotas com temporizadores
+    print("Calling remote operations with timers:")
+    result, elapsed_time = timed_operation(server.void)
+    print(f"Void operation completed in {elapsed_time:.9f} seconds")
+    perform_operation(server.long_function, 100000000)
+    perform_operation(server.multiple_long_function, 100000000, 100000000, 100000000, 100000000, 100000000, 100000000, 100000000, 100000000)
+    perform_operation(server.string_operation, "hello world")
+    complex_value = ComplexType(1, "matheus")
+    perform_operation(server.complex_operation, complex_value)
+    complex_value2 = ComplexType2(
+        id=123,
+        age=30,
+        address="123 Main St",
+        name="John Doe",
+        food="Pizza",
+        color="Blue",
+        game="Chess",
+        sport="Soccer",
+        height="6ft 2in",
+        nickname="Johnny"
+    )
+    perform_operation(server.complex_operation2, complex_value2)
+    perform_operation(server.multiple_string, "hello", "world", "helloworld", "helloworld", "helloworld", "helloworld")
+    print("\n")
--- server.py
+++ server.py
@@ -5,6 +5,19 @@ class ComplexType:
     def __init__(self, number, string):
         self.number = number
         self.string = string
+        
+class ComplexType2:
+    def __init__(self, id, age, address, name, food, color, game, sport, height, nickname):
+        self.id = id
+        self.age = age
+        self.address = address
+        self.name = name
+        self.food = food
+        self.color = color
+        self.game = game
+        self.sport = sport
+        self.height = height
+        self.nickname = nickname
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -29,8 +42,12 @@ def string_operation(value):
 def complex_operation(complex_value):
     return ComplexType(complex_value["number"] + 1, complex_value["string"])
 
+def complex_operation2(complex_value):
+    return ComplexType2(complex_value["id"] + 1, complex_value["age"] + 1, complex_value["address"], complex_value["name"], complex_value["food"], complex_value["color"], complex_value["game"], complex_value["sport"], complex_value["height"], complex_value["nickname"])
+
 def multiple_string(s1, s2, s3, s4, s5, s6):
     return s1 + s2 + s3 + s4 + s5 + s6
+
 
 # Registrar funções
 server.register_function(void, 'void')
@@ -38,6 +55,7 @@ server.register_function(long_function, 'long_function')
 server.register_function(multiple_long_function, 'multiple_long_function')
 server.register_function(string_operation, 'string_operation')
 server.register_function(complex_operation, 'complex_operation')
+server.register_function(complex_operation2, 'complex_operation2')
 server.register_function(multiple_string, 'multiple_string')
 
 # Executar o servidor
